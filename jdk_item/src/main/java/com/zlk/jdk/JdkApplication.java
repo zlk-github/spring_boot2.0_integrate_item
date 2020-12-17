ccce49ade6c9a9d4e20df5d8e211f9b3cc8bd3fb
zhoulikuan@yiye.ai <19921105zhou>
2020-12-17 15:58:59 +0800

    feat: java项目初次提交（非框架与开源组件）

---
+++ jdk_item/src/main/java/com/zlk/jdk/JdkApplication.java
@@ -0,0 +1,13 @@
+package com.zlk.jdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdkApplication.class, args);
+    }
+
+}
---
+++ jdk_item/src/test/java/com/zlk/jdk/JdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlk.jdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
